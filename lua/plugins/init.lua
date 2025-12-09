8402560882c2d3a16135d0930f44dce451c4f625
RasmusLumholdt <dev.rlumholdt@gmail.com>
2025-12-09 07:23:25 +0100

    added initial dap and cursor smear

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,14 @@
 vim.pack.add({
     { src = "https://github.com/sainnhe/everforest" },
+    { src = "https://github.com/sphamba/smear-cursor.nvim" },
 })
 
 vim.g.everforest_enable_italic = true
 vim.g.everforest_transparent_background = 2
 vim.cmd.colorscheme("everforest")
 
+require("smear_cursor").setup({})
+
 require("plugins.snacks")
 require("plugins.minifiles")
+require("plugins.dap-config")
